dae860184a6b618b9f8eec0f69c6dc4b5f90b9f6
Nicholas Chaimov <nchaimov@paratools.com>
2025-09-17 17:37:11 -0700

    Cleanup scripts; more logging

--- containers/spindle-serial-ubuntu/scripts/build_spindle.sh
+++ containers/spindle-serial-ubuntu/scripts/build_spindle.sh
@@ -2,12 +2,9 @@
 
 set -euxo pipefail
 
-#git clone -b devel-paratools https://github.com/ParaToolsInc/Spindle.git /home/$USER/Spindle
-ls -la 
-ls -la /home/$USER/Spindle
 mkdir -p /home/$USER/Spindle-build
 cd /home/$USER/Spindle-build
-/home/$USER/Spindle/configure --prefix=/home/$USER/Spindle-inst --enable-sec-munge --with-rm=serial --with-localstorage=/tmp CFLAGS="-Og -g" CXXFLAGS="-Og -g"
-make -j8
+/home/$USER/Spindle/configure --prefix=/home/$USER/Spindle-inst --enable-sec-munge --with-rm=serial --with-localstorage=/tmp CFLAGS="-O2 -g" CXXFLAGS="-O2 -g"
+make -j$(nproc)
 make install
 
